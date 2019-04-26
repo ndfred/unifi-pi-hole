9994e90bbe471c3bc2724a4bae060f68081ad824
Frédéric Sagnes <frederic.sagnes@gmail.com>
2019-04-27 00:18:22 +0100

    Initial rules generation script

---
+++ build_rules.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib2
+import re
+
+# See appendToListsFile in https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh
+# Other sources at https://firebog.net
+AD_LISTS = [
+    ('StevenBlack\'s Unified Hosts List', 'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts'),
+    ('MalwareDomains', 'https://mirror1.malwaredomains.com/files/justdomains'),
+    ('Cameleon', 'http://sysctl.org/cameleon/hosts'),
+    ('ZeusTracker', 'https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist'),
+    ('Disconnect.me Tracking', 'https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt'),
+    ('Disconnect.me Ads', 'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt'),
+    ('Hosts-file.net Ads', 'https://hosts-file.net/ad_servers.txt'),
+]
+
+DOMAIN_EXPR = re.compile(r'^[a-zA-Z0-9\.\-]+$')
+ZERO_IP_PREFIXES = ('0.0.0.0 ', '127.0.0.1 ')
+INVALID_DOMAINS = frozenset(('localhost', '0.0.0.0'))
+
+def cleanup_domain_line(line):
+    if '#' in line:
+        line = line[:line.index('#')]
+
+    line = line.strip().replace('\t ', ' ').replace('\t', ' ')
+
+    return line
+
+def parse_domain_line(line):
+    line = cleanup_domain_line(line)
+    domain = None
+
+    if DOMAIN_EXPR.match(line):
+        domain = line
+    else:
+        for prefix in ZERO_IP_PREFIXES:
+            if line.startswith(prefix):
+                domain = line[len(prefix):].strip()
+
+                if domain in INVALID_DOMAINS:
+                    domain = None
+
+    return domain
+
+def download_file(url):
+    return urllib2.urlopen(url).read()
+
+def parse_host_file(url):
+    for line in download_file(url).split('\n'):
+        domain = parse_domain_line(line)
+
+        if domain:
+            yield domain
+
+def output_rules(configuration_script_path):
+    prefix = 'service dns forwarding blacklist'
+    domains_buffer = []
+
+    for name, url in AD_LISTS:
+        print 'Parsing %s' % name
+
+        for domain in parse_host_file(url):
+            if domain.count('.') > 1:
+                domains_buffer.append('set %s hosts exclude %s' % (prefix, domain))
+            else:
+                domains_buffer.append('set %s exclude %s' % (prefix, domain))
+
+    domains_buffer = sorted(set(domains_buffer))
+
+    with open(configuration_script_path, 'w') as config_script:
+        config_script.write('configure\n')
+        config_script.write('delete %s\n' % prefix)
+        config_script.write('set %s dns-redirect-ip 0.0.0.0\n' % prefix)
+        config_script.write('\n'.join(domains_buffer))
+        config_script.write('\ncommit; save; exit\n')
+
+    print 'Wrote %d host names in %s' % (len(domains_buffer), configuration_script_path)
+
+    return 0
+
+def main():
+    output_rules('configure.sh')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ build_rules_tests.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from build_rules import *
+import unittest
+
+class TestDomainParsing(unittest.TestCase):
+    def test_domain_expr(self):
+        self.assertFalse(DOMAIN_EXPR.match('Hello world'))
+        self.assertFalse(DOMAIN_EXPR.match('https://www.google.com'))
+        self.assertTrue(DOMAIN_EXPR.match('google.com'))
+        self.assertTrue(DOMAIN_EXPR.match('my-google.com'))
+        self.assertTrue(DOMAIN_EXPR.match('sub.my-google.com'))
+        self.assertTrue(DOMAIN_EXPR.match('sub.google.com'))
+
+    def test_cleanup_domain_line(self):
+        self.assertEqual(cleanup_domain_line(' '), '')
+        self.assertEqual(cleanup_domain_line('# Comment'), '')
+        self.assertEqual(cleanup_domain_line('domain.com # Comment'), 'domain.com')
+        self.assertEqual(cleanup_domain_line(' domain.com # Comment'), 'domain.com')
+        self.assertEqual(cleanup_domain_line('0.0.0.0 domain.com # Comment'), '0.0.0.0 domain.com')
+        self.assertEqual(cleanup_domain_line('0.0.0.0\t domain.com # Comment'), '0.0.0.0 domain.com')
+
+    def test_StevenBlack(self):
+        self.assertIsNone(parse_domain_line('# This hosts file is a merged collection of hosts from reputable sources,'))
+        self.assertIsNone(parse_domain_line('# ==============================================================='))
+        self.assertIsNone(parse_domain_line('127.0.0.1 localhost'))
+        self.assertIsNone(parse_domain_line('::1 localhost'))
+        self.assertIsNone(parse_domain_line('0.0.0.0 0.0.0.0'))
+        self.assertEqual(parse_domain_line('0.0.0.0 1493361689.rsc.cdn77.org'), '1493361689.rsc.cdn77.org')
+        self.assertEqual(parse_domain_line('0.0.0.0 123greetings.com  # contains one link to distributor of adware or spyware'), '123greetings.com')
+        
+
+    def test_MalwareDomains(self):
+        self.assertIsNone(parse_domain_line(''))
+        self.assertEqual(parse_domain_line('amazon.co.uk.security-check.ga'), 'amazon.co.uk.security-check.ga')
+
+    def test_Cameleon(self):
+        self.assertIsNone(parse_domain_line('# Last updated : 2018-03-17'))
+        self.assertIsNone(parse_domain_line('127.0.0.1        localhost'))
+        self.assertEqual(parse_domain_line('127.0.0.1	 0.r.msn.com'), '0.r.msn.com')
+        self.assertEqual(parse_domain_line('127.0.0.1        0.r.msn.com'), '0.r.msn.com')
+
+    def test_ZeusTracker(self):
+        self.assertIsNone(parse_domain_line('##############################################################################'))
+        self.assertEqual(parse_domain_line('039b1ee.netsolhost.com'), '039b1ee.netsolhost.com')
+
+    def test_Disconnect(self):
+        self.assertIsNone(parse_domain_line('# Basic tracking list by Disconnect'))
+        self.assertEqual(parse_domain_line('bango.combango.org'), 'bango.combango.org')
+
+    def test_Hostsfile(self):
+        self.assertIsNone(parse_domain_line('# hpHosts - Ad and Tracking servers only'))
+        self.assertIsNone(parse_domain_line('# Hosts: 45737'))
+        self.assertEqual(parse_domain_line('127.0.0.1	005.free-counter.co.uk'), '005.free-counter.co.uk')
+        self.assertEqual(parse_domain_line('127.0.0.1	118d654612df63bc8395-aecfeaabe29a34ea9a877711ec6d8aed.r37.cf2.rackcdn.com'), '118d654612df63bc8395-aecfeaabe29a34ea9a877711ec6d8aed.r37.cf2.rackcdn.com')
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
